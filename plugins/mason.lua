9e6cc772cb0f975a9480ef626a3fbb6d32111c49
Blotz <f.p.theil@gmail.com>
2023-10-04 21:48:27 +0100

    Added a new readme

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -26,17 +26,19 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        -- bash 
+        -- bash
         "spellcheck",
         "shfmt",
-        -- python 
+        -- python
         "black",
         "isort",
-        -- lua 
+        -- lua
         "stylua",
+        -- prettier
+        "prettier",
       })
       if not opts.handlers then opts.handlers = {} end
-      opts.handlers.taplo = function() end 
+      opts.handlers.taplo = function() end
     end,
   },
   {
